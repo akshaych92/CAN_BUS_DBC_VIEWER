136be87e6641105229f2e93b1be9668f744eba0a
Akshay <akshay.ch.92@gmail.com>
2022-10-05 12:48:52 +0530

    Gave some error handling and removed unwanted files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from dbc import Ui_MainWindow as dbcui
 import sys
 import cantools
@@ -10,6 +11,7 @@ class UIhandler():
         self.path = ""
         self.messages = ""
         self.namelist = []
+        self.msg = QMessageBox()
 
     def itemselected(self, item: cantools.db.Message):
         message_name, id = str(item.text()).split(" ")
@@ -27,24 +29,43 @@ class UIhandler():
             row = row + 1
 
     def backbutton(self):
-        print("button pressed")
         dui.stackedWidget.setCurrentWidget(dui.welcomepage)
 
     def openbutton(self):
-        print("new button clicked")
         self.path = QtWidgets.QFileDialog.getOpenFileName()
+        filename, fileext = str(self.path[0]).split(".")
+        if fileext != "dbc":
+            self.msg.setWindowTitle("Error")
+            self.msg.setText("Not a DBC file")
+            self.msg.setIcon(QMessageBox.Critical)
+            x = self.msg.exec_()
+            return None
+
         # print(path[0])
-        self.db = cantools.database.load_file(self.path[0])
-        self.messages = self.db.messages
-        dui.stackedWidget.setCurrentWidget(dui.viewerpage)
-        self.namelist = []
-        print(self.messages[0].frame_id)
-        for i in self.messages:
-            self.namelist.append(i.name + " (" + str(hex(i.frame_id)) + ")")
-        print(self.namelist)
-        dui.MessagelistWidget.clear()
-        dui.MessagelistWidget.addItems(self.namelist)
-        print(self.namelist)
+        try:
+            self.db = cantools.database.load_file(self.path[0])
+            self.messages = self.db.messages
+            dui.stackedWidget.setCurrentWidget(dui.viewerpage)
+            self.namelist = []
+            # print(self.messages[0].frame_id)
+            for i in self.messages:
+                self.namelist.append(i.name + " (" + str(hex(i.frame_id)) + ")")
+            # print(self.namelist)
+            dui.MessagelistWidget.clear()
+            dui.MessagelistWidget.addItems(self.namelist)
+            # print(self.namelist)
+
+        except:
+            self.msg.setWindowTitle("Error")
+            self.msg.setText("Error in opening/parsing the DBC file")
+            self.msg.setIcon(QMessageBox.Critical)
+            x = self.msg.exec_()
+
+    def newimplementation(self, text):
+        self.msg.setWindowTitle(text)
+        self.msg.setText("Implementation in Progress")
+        self.msg.setIcon(QMessageBox.Information)
+        x = self.msg.exec_()
 
 
 if __name__ == '__main__':
@@ -57,5 +78,9 @@ if __name__ == '__main__':
     dui.actionOpen.triggered.connect(uihandler.openbutton)
     dui.backmenubutton.clicked.connect(uihandler.backbutton)
     dui.MessagelistWidget.itemClicked.connect(uihandler.itemselected)
+    dui.actionNew.triggered.connect(lambda: uihandler.newimplementation("New Dbc"))
+    dui.actionSave.triggered.connect(lambda: uihandler.newimplementation("Save Dbc"))
+    dui.actionCopy.triggered.connect(lambda: uihandler.newimplementation("Copy Dbc"))
+    dui.actionPaste.triggered.connect(lambda: uihandler.newimplementation("Paste Dbc"))
     mainwindow.show()
     sys.exit(app.exec_())
